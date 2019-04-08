aab71fe7c5c43241fc020f2bdb490e25777b4b1d
Lavanya <lavanya.annadanam@assurity.co.nz>
2019-04-09 10:49:04 +1200

    Add a Hashmap code

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,2 +1,17 @@
 public class Tests {
+
+    public static void main (String [] args){
+
+        HashMap<String, String> objMap = new HashMap<String, String>();
+        objMap.put("Name" , "Suzuki");
+        objMap.put("Power", "220");
+        objMap.put("Type", "2-wheeler");
+        objMap.put("Price", "850000");
+        System.out.println("Elements of the map are : ");
+        System.out.println(objMap);
+
+
+    }
+}
+
 }
